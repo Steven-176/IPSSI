b37bd4731d19185d9b3e4326c1ccb6af8bac1e22
Denis Kirat <denis.kirat@gmail.com>
2022-10-05 23:20:11 +0200

    VERSION 1.4 (JOUEURS SUCCESS / CLUB ON WORKING)

--- index.php
+++ index.php
@@ -53,15 +53,18 @@
          ***************************************/
         function getTableJoueur($table, $ligne) {
             $tab = array(
-                'Prénom Nom' => $table['records'][$ligne]['fields']['prenom']."<br />".$table['records'][$ligne]['fields']['nom'],
+                'IDJoueur' => $table['records'][$ligne]['id'],
+                'Prénom' => $table['records'][$ligne]['fields']['prenom'],
+                'NomJoueur' => $table['records'][$ligne]['fields']['nom'],
                 'Photo' => $table['records'][$ligne]['fields']['Photo'],
                 'Nationalité' => $table['records'][$ligne]['fields']['nationalite'],
                 'Poste' => $table['records'][$ligne]['fields']['poste'],
                 'Club' => $table['records'][$ligne]['fields']['nom (from club)'][0],
-                'N° Maillot' => $table['records'][$ligne]['fields']['numero_maillot'],
-                'Match Joués' => $table['records'][$ligne]['fields']['match_joues'],
-                'But Marqués' => $tab[7] = $table['records'][$ligne]['fields']['buts_marques'],
-                'Passes Décisives' => $table['records'][$ligne]['fields']['passes_decisives'],
+                'Maillot' => $table['records'][$ligne]['fields']['numero_maillot'],
+                'Matchs' => $table['records'][$ligne]['fields']['match_joues'],
+                'Buts' => $tab[7] = $table['records'][$ligne]['fields']['buts_marques'],
+                'PassesD' => $table['records'][$ligne]['fields']['passes_decisives'],
+                'IDClubJoueur' => $table['records'][$ligne]['fields']['club'][0],
             );
             return $tab;
         }
@@ -72,8 +75,8 @@
          **************************************/
         function getTableClub($table, $ligne) {
             $tab = array(
-                'ID' => $table['records'][$ligne]['id'],
-                'Nom' => $table['records'][$ligne]['fields']['nom'],
+                'IDClub' => $table['records'][$ligne]['id'],
+                'NomClub' => $table['records'][$ligne]['fields']['nom'],
                 'Logo' => $table['records'][$ligne]['fields']['Logo'],
                 'Classement' => $table['records'][$ligne]['fields']['classement'],
                 'Ville' => $table['records'][$ligne]['fields']['ville'],
@@ -132,48 +135,57 @@
          ***************************/
         echo '<div class="Joueurs"><h2 class="title">Joueurs</h2>';
 
+        $keyinfos = array_keys($tableau_joueur[0]);
+
         for ($i = 0 ; $i < count($tableau_joueur) ; $i++) {
+
             echo '<div class="Carte"><table>
+            <span id="'.$keyinfos[0].'_'.$i.'" class="joueurID">'.$tableau_joueur[$i]['IDJoueur'].'</span>
+            <span id="'.$keyinfos[3].'_'.$i.'" class="joueurID">'.$tableau_joueur[$i]['Photo'][0]['url'].'</span>
+            <span id="'.$keyinfos[11].'_'.$i.'" class="joueurID">'.$tableau_joueur[$i]['IDClubJoueur'].'</span>
             <thead>
-                <tr><th class="buttonModif" colspan="2"><button class="realButtonModif"><img class="imgIcone" src="https://cdn-icons-png.flaticon.com/512/84/84380.png"></button></th></tr>
                 <tr>
-                    <th>'.$tableau_joueur[$i]['Prénom Nom'].'</th>
-                    <th><img class="imgJoueur" src="'.$tableau_joueur[$i]['Photo'][0]['url'].'"></th>
+                    <th class="buttonModif"><button class="realButtonModif" onclick="modifPlayer('.$i.')"><img class="imgIcone" src="https://cdn-icons-png.flaticon.com/512/84/84380.png"></button></th>
+                </tr>
+                <tr>
+                    <th><span id="'.$keyinfos[1].'_'.$i.'">'.$tableau_joueur[$i]['Prénom'].'</span><br /><span id="'.$keyinfos[2].'_'.$i.'">'.$tableau_joueur[$i]['NomJoueur'].'</span></th>
+                    <th><img class="imgJoueur" src="'.$tableau_joueur[$i]['Photo'][0]['url'].'"></img></th>
                 </tr>
             </thead>
             <tbody><span class="bodyinfos">';
             foreach($tableau_joueur[$i] as $infos => $value){
-                if($infos != 'Prénom Nom' && $infos != 'Photo'){
-                    echo '<tr><td class="infos">'.$infos.' :</td><td class="value">'.$value.'</td></tr>';
+                if($infos != 'Prénom' && $infos != 'NomJoueur' && $infos != 'Photo' && $infos != 'IDJoueur' && $infos != 'IDClubJoueur'){
+                    echo '<tr><td class="infos">'.$infos.' :</td><td id="'.$infos.'_'.$i.'"class="value">'.$value.'</td></tr>';
                 }
             }
             echo '</span></tbody></table></div>';
         }
 
-        /*******************************
-         *  Formulaire ajout de joueur *
-         *******************************/
+
+        /********************************
+         *  Formulaire ajout de joueur  *
+         ********************************/
 
         echo '<div class="Carte"><form id="ajout">
         <table>
             <thead>
                 <tr>
                     <th>    
-                        <input type="text" id="prenomJoueur" placeholder="Prénom" maxlength="16" required><span class="validity"></span><br />
-                        <input type="text" id="nomJoueur" placeholder="Nom" maxlength="16" required><span class="validity"></span>    
+                        <input type="text" id="'.$keyinfos[1].'_modif" placeholder="Prénom" maxlength="16" required><span class="validity"></span><br />
+                        <input type="text" id="'.$keyinfos[2].'_modif" placeholder="Nom" maxlength="16" required><span class="validity"></span>    
                     </th>
-                    <th>    <input class="ImageFoot" id="imageJoueur" type="text" placeholder="URL image du joueur" required><span class="validity"></span></th>
+                    <th>    <input class="ImageFoot" id="'.$keyinfos[3].'_modif" type="text" placeholder="URL image du joueur" required><span class="validity"></span></th>
                 </tr>
             </thead>
             <tbody><span class="bodyinfos">';
             foreach($tableau_joueur[0] as $infos => $value){
                 switch ($infos) {
                     case 'Nationalité' :
-                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="text" maxlength="16" id="nationaliteJoueur" required><span class="validity"></span></td></tr>';
+                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="text" maxlength="16" id="'.$keyinfos[4].'_modif" required><span class="validity"></span></td></tr>';
                         break;
                     case 'Poste' :
                         echo '<tr><td class="infos">'.$infos.' :</td>
-                        <td class="value"><select id="posteJoueur" required >
+                        <td class="value"><select id="'.$keyinfos[5].'_modif" required >
                         <option value="" selected disabled hidden>Sélectionner poste</option>
                         <option value="Gardien">Gardien</option>
                         <option value="Défenseur">Défenseur</option>
@@ -183,24 +195,24 @@
                         break;                    
                     case 'Club' :
                         echo '<tr><td class="infos">'.$infos.' :</td>
-                        <td class="value"><select id="clubJoueur" required >
+                        <td class="value"><select id="'.$keyinfos[6].'_modif" required >
                         <option value="" selected disabled hidden>Sélectionner club existant</option>';    
                             foreach($tableau_club as $clubid){
-                                echo '<option value="'.$clubid['ID'].'">'.$clubid['Nom'].'</option>';
+                                echo '<option value="'.$clubid['IDClub'].'">'.$clubid['NomClub'].'</option>';
                             }
                         echo '</select><span class="validity"></span></td></tr>';
                         break;
-                    case 'N° Maillot' :
-                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="1" max="99" placeholder="1 - 99" id="maillotJoueur" required><span class="validity"></span></td></tr>';
+                    case 'Maillot' :
+                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="1" max="99" placeholder="1 - 99" id="'.$keyinfos[7].'_modif" required><span class="validity"></span></td></tr>';
                         break;
-                    case 'Match Joués' :
-                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="0" max="1000" placeholder="0 - 1000" id="matchsJouesJoueur" required><span class="validity"></span></td></tr>';
+                    case 'Matchs' :
+                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="0" max="1000" placeholder="0 - 1000" id="'.$keyinfos[8].'_modif" required><span class="validity"></span></td></tr>';
                         break;
-                    case 'But Marqués' :
-                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="0" max="1000" placeholder="0 - 1000" id="butsMarquesJoueur" required><span class="validity"></span></td></tr>';
+                    case 'Buts' :
+                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="0" max="1000" placeholder="0 - 1000" id="'.$keyinfos[9].'_modif" required><span class="validity"></span></td></tr>';
                         break;
-                    case 'Passes Décisives' :
-                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="0" max="1000" placeholder="0 - 1000" id="passesDecisivesJoueur" required><span class="validity"></span></td></tr>';
+                    case 'PassesD' :
+                        echo '<tr><td class="infos">'.$infos.' :</td><td class="value"><input type="number" min="0" max="1000" placeholder="0 - 1000" id="'.$keyinfos[10].'_modif" required><span class="validity"></span></td></tr>';
                         break;
                 }
             }
@@ -208,33 +220,69 @@
         
         echo '</tbody></table></form></div>';
 
-
         echo '</div>';
+
+
+        /*************************** 
+         *  Affichage des Joueurs  *
+         ***************************/
 
         echo '<div class="Clubs"><h2 class="title">Clubs</h2>';
 
-        for ($i = 0 ; $i < count($tableau_club) ; $i++) {
+        $keyinfos = array_keys($tableau_club[0]);
+
             echo '<div class="Equipe"><table>
             <tr>
             <theader>
-                <th rowspan="2" class="nomClub">'.$tableau_club[$i]['Nom'].'<br /><img class="imgClub" src="'.$tableau_club[$i]['Logo'][0]['url'].'"></th>
+                <th rowspan="2" class="nomClub">'.$tableau_club[$i]['NomClub'].'<br /><input class="ImageFoot" id="'.$keyinfos[3].'_modif" type="text" placeholder="URL image du joueur" required><span class="validity"></span></th>
             </theader>';
 
             foreach($tableau_club[$i] as $infos => $value){
-                if($infos != 'Nom' && $infos != 'Logo' && $infos != 'ID'){
+                if($infos != 'NomClub' && $infos != 'Logo' && $infos != 'IDClub'){
                     echo '<td class="infos">'.$infos.' :</td>';
                 }
             }            
             echo '</tr><tr>';
 
             foreach($tableau_club[$i] as $infos => $value){
-                if($infos != 'Nom' && $infos != 'Logo' && $infos != 'ID'){
+                if($infos != 'NomClub' && $infos != 'Logo' && $infos != 'IDClub'){
                     echo '<td class="value">'.$value.'</td>';
                 }
             }
             echo '</tr></table></div>';
-        }
-        echo '</div>'
+
+
+        // /******************************
+        //  *  Formulaire ajout de club  *
+        //  ******************************/
+
+        // echo '<div class="Clubs"><h2 class="title">Clubs</h2>';
+
+        // $keyinfos = array_keys($tableau_joueur[0]);
+
+        // for ($i = 0 ; $i < count($tableau_club) ; $i++) {
+        //     echo '<div class="Equipe"><table>
+        //     <tr>
+        //     <theader>
+        //         <th rowspan="2" class="nomClub"><input type="text" id="'.$keyinfos[2].'_modif" placeholder="Nom" maxlength="16" required><span class="validity"></span><br /><img class="imgClub" src="'.$tableau_club[$i]['Logo'][0]['url'].'"></th>
+        //     </theader>';
+
+        //     foreach($keyinfos as $infos){
+        //         if($infos != 'NomClub' && $infos != 'Logo' && $infos != 'IDClub'){
+        //             echo '<td class="infos">'.$infos.' :</td>';
+        //         }
+        //     }            
+        //     echo '</tr><tr>';
+
+        //     foreach($keyinfos){
+        //         if($infos != 'NomClub' && $infos != 'Logo' && $infos != 'IDClub'){
+        //             echo '<td class="value">'.$value.'</td>';
+        //         }
+        //     }
+        //     echo '</tr></table></div>';
+        // }
+
+        // echo '</div>'
     ?>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const API_KEY = 'keylGy9dvvUc5clRX';
 const URL = `https://api.airtable.com/v0/appljQzgZO6yDwebG/Joueur?api_key=${API_KEY}`;
 
+var ID;
+
 const formulaire = document.querySelector('#ajout')
 formulaire.addEventListener('submit', (e)=>{
     e.preventDefault()
@@ -8,54 +10,136 @@ formulaire.addEventListener('submit', (e)=>{
 })
 function addPlayer() {
 
-    var prenomJoueur = document.getElementById("prenomJoueur").value;
-    var nomJoueur = document.getElementById("nomJoueur").value;
-    var imageJoueur = document.getElementById("imageJoueur").value;
-    var nationaliteJoueur = document.getElementById("nationaliteJoueur").value;
-    var posteJoueur = document.getElementById("posteJoueur").value;
-    var clubJoueur = document.getElementById("clubJoueur").value;
-    var maillotJoueur = parseInt(document.getElementById("maillotJoueur").value);
-    var matchsJouesJoueur = parseInt(document.getElementById("matchsJouesJoueur").value);
-    var butsMarquesJoueur = parseInt(document.getElementById("butsMarquesJoueur").value);
-    var passesDecisivesJoueur = parseInt(document.getElementById("passesDecisivesJoueur").value);
+    var prenomJoueur = document.querySelector("#Prénom_modif").value;
+    var nomJoueur = document.querySelector("#NomJoueur_modif").value;
+    var imageJoueur = document.querySelector("#Photo_modif").value;
+    var nationaliteJoueur = document.querySelector("#Nationalité_modif").value;
+    var posteJoueur = document.querySelector("#Poste_modif").value;
+    var clubJoueur = document.querySelector("#Club_modif").value;
+    var maillotJoueur = parseInt(document.querySelector("#Maillot_modif").value);
+    var matchsJouesJoueur = parseInt(document.querySelector("#Matchs_modif").value);
+    var butsMarquesJoueur = parseInt(document.querySelector("#Buts_modif").value);
+    var passesDecisivesJoueur = parseInt(document.querySelector("#PassesD_modif").value);
 
-    let data = {
-        'records': [{
-            'fields': {
-                'nom' : nomJoueur,
-                'prenom' : prenomJoueur,
-                'poste' : posteJoueur,
-                'nationalite' : nationaliteJoueur,
-                'club' : [
-                    clubJoueur
-                ],
-                'numero_maillot' : maillotJoueur,
-                'match_joues' : matchsJouesJoueur,
-                'buts_marques' : butsMarquesJoueur,
-                'passes_decisives' : passesDecisivesJoueur,
-                'Photo' : [
-                    {
-                        "url": imageJoueur
-                    }
-                ]
+    if (ID != null) {
+        let data = {
+            'records': [{
+                'id' : ID,
+                'fields': {
+                    'nom' : nomJoueur,
+                    'prenom' : prenomJoueur,
+                    'poste' : posteJoueur,
+                    'nationalite' : nationaliteJoueur,
+                    'club' : [
+                        clubJoueur
+                    ],
+                    'numero_maillot' : maillotJoueur,
+                    'match_joues' : matchsJouesJoueur,
+                    'buts_marques' : butsMarquesJoueur,
+                    'passes_decisives' : passesDecisivesJoueur,
+                    'Photo' : [
+                        {
+                            "url": imageJoueur
+                        }
+                    ]
+                }
+            }]
+        };
+
+        fetch(URL, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if(response.ok){
+                response.json().then((data) => {
+                    console.log(data);
+                })
+            } else {
+                console.log('Erreur status != 200')
             }
-        }]
-    };
+        }).catch((error) => {
+            console.log(`Erreur : ${error.message}`);
+        })
+    }
+    else
+    {
+        let data = {
+            'records': [{
+                'fields': {
+                    'nom' : nomJoueur,
+                    'prenom' : prenomJoueur,
+                    'poste' : posteJoueur,
+                    'nationalite' : nationaliteJoueur,
+                    'club' : [
+                        clubJoueur
+                    ],
+                    'numero_maillot' : maillotJoueur,
+                    'match_joues' : matchsJouesJoueur,
+                    'buts_marques' : butsMarquesJoueur,
+                    'passes_decisives' : passesDecisivesJoueur,
+                    'Photo' : [
+                        {
+                            "url": imageJoueur
+                        }
+                    ]
+                }
+            }]
+        };
 
-fetch(URL, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    }).then((response) => {
-        if(response.ok){
-            response.json().then((data) => {
-                console.log(data);
-            })
-        } else {
-            console.log('Erreur status != 200')
-        }
-    }).catch((error) => {
-        console.log(`Erreur : ${error.message}`);
-    })
+        fetch(URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        }).then((response) => {
+            if(response.ok){
+                response.json().then((data) => {
+                    console.log(data);
+                })
+            } else {
+                console.log('Erreur status != 200')
+            }
+        }).catch((error) => {
+            console.log(`Erreur : ${error.message}`);
+        })
+    }
+}
+
+function modifPlayer(i) {
+
+    ID = document.querySelector("#IDJoueur_"+i).innerHTML;
+
+    var prenomJoueur = document.querySelector("#Prénom_"+i).innerHTML;
+    var nomJoueur = document.querySelector("#NomJoueur_"+i).innerHTML;
+    var imageJoueur = document.querySelector("#Photo_"+i).innerHTML;
+    var nationaliteJoueur = document.querySelector("#Nationalité_"+i).innerHTML;
+    var posteJoueur = document.querySelector("#Poste_"+i).innerHTML;
+    var clubJoueur = document.querySelector("#IDClubJoueur_"+i).innerHTML;
+    var maillotJoueur = parseInt(document.querySelector("#Maillot_"+i).innerHTML);
+    var matchsJouesJoueur = parseInt(document.querySelector("#Matchs_"+i).innerHTML);
+    var butsMarquesJoueur = parseInt(document.querySelector("#Buts_"+i).innerHTML);
+    var passesDecisivesJoueur = parseInt(document.querySelector("#PassesD_"+i).innerHTML);
+
+    console.log(prenomJoueur+" ; type "+typeof(prenomJoueur));
+    console.log(nomJoueur+" ; type "+typeof(nomJoueur));
+    console.log(imageJoueur+" ; type "+typeof(imageJoueur));
+    console.log(nationaliteJoueur+" ; type "+typeof(nationaliteJoueur));
+    console.log(posteJoueur+" ; type "+typeof(posteJoueur));
+    console.log(clubJoueur+" ; type "+typeof(clubJoueur));
+    console.log(maillotJoueur+" ; type "+typeof(maillotJoueur));
+    console.log(matchsJouesJoueur+" ; type "+typeof(matchsJouesJoueur));
+    console.log(butsMarquesJoueur+" ; type "+typeof(butsMarquesJoueur));
+    console.log(passesDecisivesJoueur+" ; type "+typeof(passesDecisivesJoueur));
+
+    document.querySelector("#Prénom_modif").value = prenomJoueur;
+    document.querySelector("#NomJoueur_modif").value = nomJoueur;
+    document.querySelector("#Photo_modif").value = imageJoueur;
+    document.querySelector("#Nationalité_modif").value = nationaliteJoueur;
+    document.querySelector("#Poste_modif").value = posteJoueur;
+    document.querySelector("#Club_modif").value = clubJoueur;
+    document.querySelector("#Maillot_modif").value = maillotJoueur;
+    document.querySelector("#Matchs_modif").value = matchsJouesJoueur;
+    document.querySelector("#Buts_modif").value = butsMarquesJoueur;
+    document.querySelector("#PassesD_modif").value = passesDecisivesJoueur;
 }
 
